e8208182d0b408be22fbcabe3f1aba0a00c82b97
Aakin Desai <aakindesai2020@u.northwestern.edu>
2020-04-24 22:19:28 -0500

    Update Plot.py

--- Kalman Filter/Plot.py
+++ Kalman Filter/Plot.py
@@ -1,23 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 data = np.loadtxt('kalman.csv',delimiter =',')
 x = data[:,:-1]
 z = data[:,-1:]
 
-actual = np.zeros(52)
+actual = np.zeros((51,1))
 u = 0
 
-for i in range(1,52):
+for i in range(51):
     
-    u = u + 0.2
     actual[i] = u
+    u = u + 0.2
     
 plt.figure()
 plt.plot(z,'k+',label='noisy measurements')
@@ -28,13 +23,18 @@ plt.title('Estimate vs. iteration step', fontweight='bold')
 plt.xlabel('Iteration')
 plt.ylabel('Distance')
 
-    
+error = np.zeros(52)
+error = actual - x
 
-    
+mu, std = norm.fit(error)
+plt.hist(error, bins=25, density=True, alpha=0.6, color='g')
 
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+p = norm.pdf(x, mu, std)
+plt.plot(x, p, 'k', linewidth=2)
+title = "Fit results: mu = %.2f,  std = %.2f" % (mu, std)
+plt.title(title)
 
-# In[ ]:
-
-
-
+plt.show()
 
