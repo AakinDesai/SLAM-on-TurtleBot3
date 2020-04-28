6ab8738e22ae00876e195cc81b1c9cb55cd7862e
Aakin Desai <aakindesai2020@u.northwestern.edu>
2020-04-27 21:44:44 -0500

    Create plot.py

---
+++ Extended Kalman Filter/plot.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+data = np.loadtxt('kalman.csv',delimiter =',')
+x = data[:,0]
+z = data[:,1]
+actual = data[:,2]
+    
+plt.figure()
+plt.plot(z,'k+',label='noisy measurements')
+plt.plot(x,'b-',label='a posteri estimate')
+plt.plot(actual,color='g',label='truth value')
+plt.legend()
+plt.title('Estimate vs. iteration step', fontweight='bold')
+plt.xlabel('Iteration')
+plt.ylabel('Distance')
+
+error = np.zeros(52)
+error = actual - x
+
+mu, std = norm.fit(error)
+plt.hist(error, bins=25, density=True, alpha=0.6, color='g')
+
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+p = norm.pdf(x, mu, std)
+plt.plot(x, p, 'k', linewidth=2)
+title = "Fit results: mu = %.2f,  std = %.2f" % (mu, std)
+plt.title(title)
+
+plt.show()
+
